84b43dcce2ae8273f3a7d7b782bba02a496c5734
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-06-01 21:14:52 +0330

    craete component module card with sstyles

--- components/template/CarsPage.js
+++ components/template/CarsPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './CarsPage.module.css'
+import Card from '../module/Card'
 
 
 function CarsPage({data}) {
   return (
     <div className={styles.container}>
         {data.map((car)=>(
-            <p key={car.id}> {car.name}</p>
+            <Card key={car.id} {...car}/>
         ))}
     </div>
   )
